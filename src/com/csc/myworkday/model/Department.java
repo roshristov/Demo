51c9992ae06161cbf4d84f599302c8c5c17f569f
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2015-10-08 15:43:35 +0300

    Employee and Department introduced
    Rough drafts of model classes

---
+++ src/com/csc/myworkday/model/Department.java
@@ -0,0 +1,5 @@
+package com.csc.myworkday.model;
+
+public class Department {
+
+}
---
+++ src/com/csc/myworkday/model/Employee.java
@@ -0,0 +1,28 @@
+package com.csc.myworkday.model;
+
+public class Employee {
+  private String name;
+  private String jobPosition;
+  private String department;
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getJobPosition() {
+    return jobPosition;
+  }
+  public void setJobPosition(String jobPosition) {
+    this.jobPosition = jobPosition;
+  }
+  public String getDepartment() {
+    return department;
+  }
+  public void setDepartment(String department) {
+    this.department = department;
+  }
+  
+  
+  
+}
