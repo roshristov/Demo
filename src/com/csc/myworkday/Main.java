e33c50c08e06ab6f6549788b7dafe65715e7de0f
Rosen <ros.hristov@gmail.com>
2015-10-08 15:48:15 +0300

    Main became MainFrame

--- src/com/csc/myworkday/Main.java
+++ src/com/csc/myworkday/Main.java
@@ -1,8 +1,17 @@
 package com.csc.myworkday;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
   public static void main(String[] args) {
     System.out.println("hello, world!");
+    Main mainFrame = new Main();
     //test
+    mainFrame.setBounds(100,100,600,300);
+    
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    mainFrame.setVisible(true);
+    
   }
 }
